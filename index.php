8baa35de784d8be88c95e902862f86b428e550eb
rahullodhi3636 <rahullodhi3636@gmail.com>
2022-07-10 22:12:18 +0530

    second commit to master

--- index.php
+++ index.php
@@ -13,6 +13,6 @@ $solution = new Solution();
 $output = $solution->subsets($nums);
 $new = array_merge($output,$nums);
 
-//commit on first on master
+//commit on first on master dddd
 
 print_r($new);
